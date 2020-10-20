bc559b4e7e4a4174eb40ecc4242f126da2551f8f
Chaman7724 <chamangupta31599@gmail.com>
2020-10-20 10:38:30 +0530

    DirectWhatsappMessage

---
+++ app/src/main/java/com/example/whatsappopener/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.whatsappopener
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.text.isDigitsOnly as isDigitsOnly1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number:String ="0"
+
+        if(intent.action == Intent.ACTION_PROCESS_TEXT){
+            number =intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
+        }
+        if(number.isDigitsOnly1()){
+            startWhatsapp(number)
+
+        }
+        else{
+            Toast.makeText(this,"please check the number",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startWhatsapp(number: String) {
+        val intent=Intent(Intent.ACTION_VIEW)
+        intent.setPackage("com.whatsapp")
+       val data:String = if(number[0] == '+'){
+            number.substring(1)
+        }
+        else if(number.length == 10){
+            "91" + number
+        }
+        else{
+            number
+        }
+        intent.data = Uri.parse("https://wa.me/$data")
+        if(packageManager.resolveActivity(intent,0)!=null){
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this,"please install whatsapp",Toast.LENGTH_SHORT).show()
+        }
+        
+
+    }
+
+
+}
